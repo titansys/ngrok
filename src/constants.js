b79eb91013eab8c51cfccdb8abc6faad8ceb06e6
Norielle Cruz <nogats07@gmail.com>
2024-03-30 01:25:57 +0800

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,9 @@
 const { join } = require("path");
 const platform = require("os").platform();
+const arch = require("os").arch();
 
-const defaultDir = join(__dirname, "..", "bin");
-const bin = platform === "win32" ? "ngrok.exe" : "ngrok";
+const defaultDir = join(process.cwd(), "storage", "ngrok");
+const bin = platform === "win32" ? "ngrok.exe" : (arch == "arm64" ? "ngrok-arm64" : "ngrok");
 const ready = /starting web service.*addr=(\d+\.\d+\.\d+\.\d+:\d+)/;
 const inUse = /address already in use/;
 
